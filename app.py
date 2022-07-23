de959e2195ccb53cdf1bb27d9c0a9cf72e4d179b
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-07-23 17:35:44 -0400

    use templates

--- app.py
+++ app.py
@@ -1,36 +1,25 @@
 from html import escape
-from flask import url_for, Flask
+from flask import render_template, url_for, Flask
 from os import system
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route('/crispresso')
-@app.route('/crispresso/<command>')
-def home(command = 'none'):
+def index():
+    return render_template( "index.html", status=" ... " )
 
-    if( command != 'none' ):
-        crispresso('-h')
+@app.route("/run")
+def run():
+    render_template( "index.html", status="Running ...")
+    if( crispresso('-h') == 0):
+        return render_template( "index.html", status="CRISPResso is done")
+    return index()
 
-    url_for('static', filename='static/style.css')
-    return f"""
-        <html>
-        <head>
-            <title>CRISPResso UI</title>
-            <link rel="stylesheet" href="/static/style.css"/>
-        </head>
-        <body>
-        <div class="container">
-            <h1>Hello, World!</h1>
-            <p>This is the upcoming CRISPResso UI.</p>
-            <p>Current command: { escape(command)}</p>
-
-            <a href="/crispresso/help">Get help</a>
-            <a href="/crispresso">Run</a>
-        </div>
-        </body>
-        </html>
-        """
+@app.route("/check")
+def check():
+    if( crispresso('-h') == 0):
+        return render_template( "index.html", status="CRISPResso is working!")
+    return index()
 
 def crispresso( args ):
-    system(f'CRISPResso {args}')
+    return system(f'CRISPResso {args}')
