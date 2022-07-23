f329f797aef821c94db7007f3811c048fb32bf69
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-07-22 20:45:04 -0400

    call CRISPResso using system call

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from html import escape
 from flask import url_for, Flask
+from os import system
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route('/run/<command>')
+@app.route('/crispresso')
+@app.route('/crispresso/<command>')
 def home(command = 'none'):
+
+    if( command != 'none' ):
+        crispresso('-h')
+
     url_for('static', filename='static/style.css')
     return f"""
         <html>
@@ -17,9 +23,14 @@ def home(command = 'none'):
         <div class="container">
             <h1>Hello, World!</h1>
             <p>This is the upcoming CRISPResso UI.</p>
-            <a href="/run/analysis">Run analysis</a>
             <p>Current command: { escape(command)}</p>
+
+            <a href="/crispresso/help">Get help</a>
+            <a href="/crispresso">Run</a>
         </div>
         </body>
         </html>
         """
+
+def crispresso( args ):
+    system(f'CRISPResso ${args}')
