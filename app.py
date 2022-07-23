87db5ebd0270f4f05c1d9aa7feb2a89012803fe7
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-07-23 18:04:00 -0400

    quick html/css improvements

--- app.py
+++ app.py
@@ -5,20 +5,24 @@ from os import system
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template( "index.html", status=" ... " )
+def index( status = " ... "):
+    return render_template( "index.html", status=f"{status}" )
+
+@app.route("/form")
+def form():
+    return render_template( "form.html" )
 
 @app.route("/run")
 def run():
     render_template( "index.html", status="Running ...")
     if( crispresso('-h') == 0):
-        return render_template( "index.html", status="CRISPResso is done")
+        return index("CRISPResso is done")
     return index()
 
 @app.route("/check")
 def check():
     if( crispresso('-h') == 0):
-        return render_template( "index.html", status="CRISPResso is working!")
+        return index("CRISPResso is working!")
     return index()
 
 def crispresso( args ):
