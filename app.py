074576e7b6880ca17e59bfce70078d5c6560d58b
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-07-23 20:14:05 -0400

    web app trigger CRISPResso

--- app.py
+++ app.py
@@ -1,29 +1,76 @@
-from html import escape
-from flask import render_template, url_for, Flask
-from os import system
+from flask import render_template, Flask, flash, request
+from os import system, path
 
 app = Flask(__name__)
 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/' # Required by flash
+
+FASTQ_R1_PATH  = "./uploads/fastq_r1"
+FASTQ_R2_PATH  = "./uploads/fastq_r2"
+OUTPUT_FOLDER  = "./output"
+
 @app.route("/")
-def index( status = " ... "):
-    return render_template( "index.html", status=f"{status}" )
+def index(status=" ... "):
+    return render_template("index.html", status=f"{status}")
+
 
 @app.route("/form")
 def form():
-    return render_template( "form.html" )
+    return render_template("form.html")
 
-@app.route("/run")
+
+@app.route("/run", methods =["GET", "POST"])
 def run():
-    render_template( "index.html", status="Running ...")
-    if( crispresso('-h') == 0):
-        return index("CRISPResso is done")
-    return index()
+    flash('Running CRISPResso -h command ...')
+
+    if request.method == "POST":
+
+        fastq_r1 = request.files['fastq_r1']
+        fastq_r2 = request.files['fastq_r2']
+
+        amplicon_seq    = request.form.get("amplicon_seq")
+        guide_seq       = request.form.get("guide_seq")
+
+        args = ''
+
+        if not fastq_r1:                                # fastq_r1
+            flash('No fastq_r1 file', 'error')
+        else:
+            fastq_r1.save(FASTQ_R1_PATH)
+            args += f' --fastq_r1 {FASTQ_R1_PATH}'
+
+        if not fastq_r2:                                # fastq_r2
+            flash('No fastq_r2 file', 'error')
+        else:
+            fastq_r1.save(FASTQ_R2_PATH)
+            args += f' --fastq_r2 {FASTQ_R2_PATH}'
+
+        if not amplicon_seq:                            # amplicon_seq
+            flash('No amplicon_seq file', 'error')
+        else:
+            args += f' --amplicon_seq {amplicon_seq}'
+
+        if not guide_seq:                               # guide_seq
+            flash('No guide_seq file', 'error')
+        else:
+            args += f' --guide_seq {guide_seq}'
+
+        args += f" --output_folder {OUTPUT_FOLDER}"
+
+        if fastq_r1 and fastq_r2 and amplicon_seq and guide_seq and crispresso(args) == 0 :
+            return index("Finished!")
+        return index("Unable to run!")
+    else:
+        return index("Require to POST data.")
 
 @app.route("/check")
 def check():
-    if( crispresso('-h') == 0):
+    if(crispresso('-h') == 0):
         return index("CRISPResso is working!")
     return index()
 
-def crispresso( args ):
-    return system(f'CRISPResso {args}')
+
+def crispresso(args ):
+    command = f'CRISPResso {args}'
+    flash(f"Running {command}")
+    return system( command )
